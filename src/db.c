e7d615f496db8bc073a041711373fcb890a6b756
DeboDevelop <debajyotidofficial@gmail.com>
2022-03-18 00:26:18 +0530

    Handled negative indexs.

--- src/db.c
+++ src/db.c
@@ -28,6 +28,7 @@ typedef enum {
 
 typedef enum {
     PREPARE_SUCCESS,
+    PREPARE_NEGATIVE_ID,
     PREPARE_UNRECOGNIZED_STATEMENT,
     PREPARE_SYNTAX_ERROR,
     PREPARE_STRING_TOO_LONG
@@ -136,6 +137,9 @@ PrepareResult prepare_insert(InputBuffer* input_buffer, Statement* statement) {
     }
 
     int id = atoi(id_string);
+    if (id < 0) {
+        return PREPARE_NEGATIVE_ID;
+    }
     if (strlen(username) > COLUMN_USERNAME_SIZE) {
         return PREPARE_STRING_TOO_LONG;
     }
@@ -257,6 +261,9 @@ int main(int argc, char* argv[]) {
         switch (prepare_statement(input_buffer, &statement)) {
             case (PREPARE_SUCCESS):
                 break;
+            case (PREPARE_NEGATIVE_ID):
+                printf("ID must be positive.\n");
+                continue;
             case (PREPARE_STRING_TOO_LONG):
                 printf("String is too long.\n");
                 continue;
